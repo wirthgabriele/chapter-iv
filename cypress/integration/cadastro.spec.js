0480de342d986bd94970615b285d6af1f2dee2b9
Gabriele Wirth <wirthgabrielew@gmail.com>
2021-10-17 21:22:50 -0300

    test: implementando testes de cadastro e listagem

---
+++ cypress/integration/cadastro.spec.js
@@ -0,0 +1,32 @@
+/// <reference types="cypress" />
+
+var Chance = require('chance');
+var chance = new Chance();
+
+describe('Cadastro', () => {
+    it('Quando eu informar os dados e finalizar, então o cadastro deve ser efetuado', () => {
+        cy.visit('https://form-agilizei.netlify.app')
+
+        cy.get('input[name=firstName]').type(chance.first())
+        cy.get('input[name=lastName]').type(chance.last())
+        cy.get('textarea[name=adress]').type(chance.address())
+        cy.get('input[name=emailAdress]').type(chance.email())
+
+        cy.get('input[value=n]').check()
+        cy.get('input[type=checkbox]').check('Netflix')
+        cy.get('input[type=checkbox]').check('Dormir')
+
+        cy.get('select#countries').select('Dinamarca', { force: true })
+        cy.get('select#years').select('2006', { force: true })
+        cy.get('select#months').select('Janeiro', { force: true })
+        cy.get('select#days').select('8', { force: true })
+
+        cy.get('input#firstpassword').type('Alunos@2021')
+        cy.get('input#secondpassword').type('Alunos@2021')
+
+        cy.contains('Finalizar cadastro').click()
+
+        cy.url().should('contain', 'listagem')
+
+    });
+});
---
+++ cypress/integration/listagem.spec.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+describe('Listagem', () => {
+    it('Quando não houver cadastros, então a listagem deve estar vazia', () => {
+        cy.fixture('listagem-vazia').then(data => {
+            window.localStorage.setItem('data', JSON.stringify(data))
+        })
+
+        cy.visit("https://form-agilizei.netlify.app/listagem.html")
+
+        cy.get('table tbody tr').should('have.length', 0)
+    });
+
+    it('Quando houver um ou mais cadastros, então a listagem deve exibir cada registro', () => {
+        cy.fixture('listagem-com-itens').then(data => {
+            window.localStorage.setItem('data', JSON.stringify(data))
+        })
+
+        cy.visit("https://form-agilizei.netlify.app/listagem.html")
+
+        cy.get('table tbody tr').should('have.length', 2)
+    });
+});
